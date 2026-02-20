d39accac26d11584c5e7f421621a9675864dc94a
Sanjeev Midda <s.midda1@gmail.com>
2026-02-20 19:50:31 +0000

    created array to map over no of switches for UI

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import "./index.css";
+import Switch from "./components/Switch";
+
+const noOfSwitches = Array.from({ length: 5 });
+
+console.log(noOfSwitches);
 
 function App() {
-  return <div className="App"></div>;
+  // Create empty array to map over
+
+  return (
+    <div className="App">
+      <div className="mainContainer">
+        {noOfSwitches.map((v, index) => (
+          <Switch key={index} />
+        ))}
+        <Switch />
+      </div>
+    </div>
+  );
 }
 
 export default App;
