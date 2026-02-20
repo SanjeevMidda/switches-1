ec3d1f168dd10e3c2d0549fb418f9793370e3de1
Sanjeev Midda <s.midda1@gmail.com>
2026-02-20 19:34:11 +0000

    created logic to change background color and position using state

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -5,10 +5,19 @@ import React, { useState } from "react";
 const Switch = () => {
   const [selected, setSelected] = useState(false);
 
+  const changeColor = () => setSelected(!selected);
+
   return (
     <div className="switchContainer">
       <div className="switchHolder">
-        <div className="switch"></div>
+        <div
+          className="switch"
+          style={{
+            background: selected ? "blue" : "greenYellow",
+            transform: `translateX(${selected ? "0" : "36px"})`,
+          }}
+          onClick={changeColor}
+        ></div>
       </div>
     </div>
   );
