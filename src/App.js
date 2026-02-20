630bd71707e8d5073082e2eae928645087eeed2a
Sanjeev Midda <s.midda1@gmail.com>
2026-02-20 20:42:20 +0000

    Refactored css to include variables for flexiblity and readability

--- src/App.js
+++ src/App.js
@@ -3,11 +3,7 @@ import Switch from "./components/Switch";
 
 const noOfSwitches = Array.from({ length: 19 });
 
-console.log(noOfSwitches);
-
 function App() {
-  // Create empty array to map over
-
   return (
     <div className="App">
       <h4>switches.</h4>
@@ -15,7 +11,6 @@ function App() {
         {noOfSwitches.map((v, index) => (
           <Switch key={index} />
         ))}
-        <Switch />
       </div>
     </div>
   );
--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -3,14 +3,14 @@ import React, { useState } from "react";
 const Switch = () => {
   const [isOn, setIsOn] = useState(false);
 
-  const changeColor = () => setIsOn((prev) => !prev);
+  const handleToggle = () => setIsOn((prev) => !prev);
 
   return (
     <div className="switchContainer">
       <div className="switchHolder">
         <button
           className={`switch ${isOn ? "switchOn" : "switchOff"}`}
-          onClick={changeColor}
+          onClick={handleToggle}
           role="switch"
           aria-checked={isOn}
         ></button>
