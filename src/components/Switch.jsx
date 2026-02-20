fc21fece020cff71d3de1b489f1fb19a589b7626
Sanjeev Midda <s.midda1@gmail.com>
2026-02-20 19:23:19 +0000

    created component to hold UI for switch

---
+++ src/components/Switch.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+// create dynamic color
+
+const Switch = () => {
+  return (
+    <div className="switchContainer">
+      <div className="switchHolder">
+        <div className="switch"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Switch;
